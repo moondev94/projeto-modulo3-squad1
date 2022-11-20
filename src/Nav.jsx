96ddf37e649531d56edd5860fecbfff52f7c3f8a
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2022-11-20 11:59:52 -0300

    Add files via upload

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,39 +1,57 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import {Nav, NavItem, NavLink} from 'reactstrap';
 
-function NavMenu() {
-    return (
-    <Nav pills
-    justified>
-        <NavItem>
-            <NavLink
-                active
-                href="#"
-            >
-                Link
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="#">
-                Link
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href="#">
-                Another Link
-            </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink
-                disabled
-                href="#"
-            >
-                Disabled Link
-            </NavLink>
-        </NavItem>
-    </Nav>
-    )
+import {useState } from 'react';
+import './Nav.css';
+import React from 'react';
+import logo from './imagens/logo.png';
+
+ function NavMenu(){
+return(
+  <div className='Header'>
+    <div className='HeaderLogo'>
+    </div>
+  <header>
+    <div className="w50">
+    <a href=""><img src={logo}/></a> 
+    </div>
+    <div className="w50">
+      <div class="menu">
+        <a href="">Início</a>
+        <a href="">Top 10 filmes</a>
+        <a href="">em Cartaz</a>
+        <a href="">Lançamentos</a>
+      </div>
+    </div>
+  </header>
+
+  <section className="banner">
+    <h2>Comprar Agora</h2>
+  </section>
+
+  <section className="diferenciais">
+    <div class="w33">
+      <h2>Texto de exemplo</h2>
+      <p>Conteúdo de exemplo</p>
+    </div>
+    <div className="w33">
+      <h2>Texto de exemplo</h2>
+      <p>Conteúdo de exemplo</p>
+    </div>
+    <div className="w33">
+      <h2>Texto de exemplo</h2>
+      <p>Conteúdo de exemplo</p>
+    </div>
+  </section>
+
+  <footer>
+    <p>Todos os direitos reservados</p>
+  </footer>
+
+</div>
+
+
+
+)
 }
+
 
 export default NavMenu;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import NavMenu from './Nav';
-import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavMenu/>
-    <App/>
-    <Footer/>
+    <NavMenu />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
