0f70fa98db89d243ff6fd8a525f20a2cf8f01d2f
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-11-20 09:52:26 -0300

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {'Card', 'CardFooter'} from 'reactstrap';
+
+function Footer(){
+  return (
+    <Card
+  className="my-2"
+  style={{
+    width: '18rem'
+  }}
+>
+  <CardFooter>
+    Footer
+  </CardFooter>
+</Card>
+
+export default Footer;
