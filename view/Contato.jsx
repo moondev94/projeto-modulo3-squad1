45a7d38b15b1ef438a3ccccdf978c2c139fb6cc8
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-11-24 17:53:44 -0300

    Páginas de rota adicionadas

---
+++ view/Contato.jsx
@@ -0,0 +1,5 @@
+const Contato = () => {
+    return <h1>Home</h1>;
+  };
+  
+  export default Contato;
---
+++ view/Filmes.jsx
@@ -0,0 +1,5 @@
+const Filmes = () => {
+    return <h1>Home</h1>;
+  };
+  
+  export default Filmes;
