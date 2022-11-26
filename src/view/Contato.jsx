e44ce8d2bd5f3b8bdfa901e96ab5923a2701031d
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-11-25 21:40:28 -0300

    Formulário de contato criado

--- src/view/Contato.jsx
+++ src/view/Contato.jsx
@@ -1,5 +1,28 @@
+import "./Contato.css"
+
 const Contato = () => {
-    return <h1>Contato</h1>;
-  };
-  
-  export default Contato;
+
+  return (
+    <body>
+    <form name="meu_form">
+    
+      <h1>Entre em contato</h1>
+      <label for="nome">Nome</label>
+      <input type="text" id="nomeid" placeholder="João Silva" required="required" name="nome" />
+      
+      <label for="fone">Fone</label>
+      <input type="tel" id="foneid" placeholder="(XX) X XXXX-XXXX" name="fone" />
+      
+      <label for="email">Email</label>
+      <input type="email" id="emailid" placeholder="joaosilva@email.com" name="email" />
+      
+      <label for="msg">Mensagem</label>
+      <textarea placeholder="Deixe sua mensagem"></textarea>
+    
+      <input type="submit" class="enviar" onclick="Enviar();" value="Enviar" />
+    </form>
+    </body>
+  );
+};
+
+export default Contato;
