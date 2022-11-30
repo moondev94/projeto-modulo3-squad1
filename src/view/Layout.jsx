ffd171eee2e252ffa5f7952ba6c5b7ba2d55aa1b
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-11-30 20:58:06 -0300

    footer removido

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -55,41 +55,6 @@ const Layout = () => {
                 </NavbarBrand>
             </Navbar>
 
-
-
-
-            <Row className="Footer">
-                <footer class="footer">
-                    <Container className="container">
-                        <Row className="row">
-                            <Col className="footer-col">
-                                <h4>Sobre</h4>
-                                <ul>
-                                    <li><a href="#">Quem Somos</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="#">Política de Privacidade</a></li>
-                                    <li><a href="#"></a></li>
-                                    <li><a href="#"></a></li>
-                                </ul>
-                            </Col>
-                            <Col className="footer-col">
-                                <h4>follow us</h4>
-                                <Col className="social-links">
-
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                                </Col>
-                            </Col>
-                            <Col className="footer-col">
-                                <a>© Popcorn TV (2022). Todos os direitos reservados.</a>
-                            </Col>
-                        </Row>
-                    </Container>
-                </footer>
-            </Row>
-
             <Outlet />
 
         </>
