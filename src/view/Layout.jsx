06a094737e6bf0a0ac941e6283cdbc74f9746328
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-12-05 22:21:55 -0300

    Estilo do layout atualizado

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -14,48 +14,45 @@ const Layout = () => {
                 <Row className="row">
                     <NavbarBrand className="Navlogo" href="/">
                         <img
-                            alt="Logo"
+                            alt="DevPlay TV"
                             src={Logo}
                             style={{
-                                height: 205,
-                                width: 375,
+                                height: 169,
+                                width: 285,
                             }} />
-
-                        <div className="NavMenu">
-                            <Button className="Btn"
-                                color="none"
-                                href="/"
-                                tag="a">
-                                <a>Home</a>
-                            </Button>
-                            {' '}
-                            <Button className="Btn"
-                                color="none"
-                                href="/Filmes"
-                                tag="a">
-                                <a>Filmes</a>
-                            </Button>
-                            {' '}
-                            <Button className="Btn"
-                                color="none"
-                                href="/Sobre"
-                                tag="a">
-                                <a>Sobre</a>
-                            </Button>
-                            {' '}
-                            <Button className="Btn"
-                                color="none"
-                                href="/Contato"
-                                tag="a">
-                                <a>Contato</a>
-                            </Button>
-                            {' '}
-
-                        </div>
-
-
-
                     </NavbarBrand>
+                    <div className="NavMenu">
+                        <Button className="Btn"
+                            color="none"
+                            href="/"
+                            tag="a">
+                            <a>Home</a>
+                        </Button>
+                        {' '}
+                        <Button className="Btn"
+                            color="none"
+                            href="/Filmes"
+                            tag="a">
+                            <a>Filmes</a>
+                        </Button>
+                        {' '}
+                        <Button className="Btn"
+                            color="none"
+                            href="/Sobre"
+                            tag="a">
+                            <a>Sobre</a>
+                        </Button>
+                        {' '}
+                        <Button className="Btn"
+                            color="none"
+                            href="/Contato"
+                            tag="a">
+                            <a>Contato</a>
+                        </Button>
+                        {' '}
+
+                    </div>
+
                 </Row>
 
             </Navbar>
