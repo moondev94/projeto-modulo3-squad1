efb5bdf20979ed7cff3023c7a1551b3206100f01
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2022-12-05 21:09:07 -0300

    Primeiro Commit

--- src/view/Sobre.jsx
+++ src/view/Sobre.jsx
@@ -1,5 +1,157 @@
+import React from 'react';
+
+import { Container, Row, Col, Toast, ToastHeader, ToastBody } from 'reactstrap';
+
 const Sobre = () => {
-    return <h1>Sobre</h1>;
-  };
-  
-  export default Sobre;
+
+return (
+
+<div>
+
+<h1>Quem Somos</h1>
+
+<div
+
+className="p-3 my-2 rounded"
+
+style={{
+
+background: 'black'
+
+}}
+
+>
+
+<Toast>
+
+<ToastBody>
+
+Nosso site de filmes foi criado para pessoas viciadas e apaixonadas por filmes. No nosso site você fica sabendo quais filmes são lançados, os que estão no cinema e aqueles que já estão em DVD, para você assistir em casa.
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+<Container>
+
+<Row>
+
+<Col className="">
+
+<div className="p-3 my-2 rounded"  style={{
+
+background: 'red'
+
+}}>
+
+<Toast>
+
+<ToastBody>
+
+Nosso site de filmes foi criado para pessoas viciadas e apaixonadas por filmes. No nosso site você fica sabendo quais filmes são lançados, os que estão no cinema e aqueles que já estão em DVD, para você assistir em casa.
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+</Col>
+
+</Row>
+
+<Row>
+
+<Col>
+
+<div className="p-3 my-2 rounded bg-docs-transparent-grid" style={{
+
+background: 'black'
+
+}}>
+
+<Toast>
+
+<ToastBody>
+
+No nosso site é entreter o mundo, e é por isso que levamos até você os melhores filmes para computadores, notebooks, celulares, tablets ou outros dispositivos. Nossos clientes controlam o que querem ver e quando, sem anúncios, com uma única assinatura.
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+</Col>
+
+<Col>
+
+<div className="p-3 my-2 rounded bg-docs-transparent-grid" style={{
+
+background: 'black'
+
+}} >
+
+<Toast>
+
+<ToastBody>
+
+No nosso site é entreter o mundo, e é por isso que levamos até você os melhores filmes para computadores, notebooks, celulares, tablets ou outros dispositivos. Nossos clientes controlam o que querem ver e quando, sem anúncios, com uma única assinatura.
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+</Col>
+
+<Col>
+
+<div className="p-3 bg-secondary my-2 rounded">
+
+<Toast>
+
+<ToastBody>
+
+This is a toast on a secondary background — check it out!
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+</Col>
+
+<Col>
+
+<div className="p-3 bg-success my-2 rounded">
+
+<Toast>
+
+<ToastBody>
+
+This is a toast on a success background — check it out!
+
+</ToastBody>
+
+</Toast>
+
+</div>
+
+</Col>
+
+</Row>
+
+</Container>
+
+</div>
+
+)
+
+};
+
+export default Sobre;
