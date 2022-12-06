480ead21ac81e0a8d8f118b7b13888ee91067f59
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2022-12-06 20:19:53 -0300

    Update Sobre.jsx

--- src/view/Sobre.jsx
+++ src/view/Sobre.jsx
@@ -4,50 +4,32 @@ import { Container, Row, Col, Toast, ToastHeader, ToastBody } from 'reactstrap';
 const Sobre = () => {
   return (
     <div>
-      <h1 className="p-3 my-2">Quem Somos</h1>
-
-      <div className="p-3 my-2 rounded">
-    <Toast>
-      <ToastBody>
-        This is a toast on a white background — check it out!
-      </ToastBody>
-    </Toast>
-  </div>
+      <h1 className="">Quem Somos</h1>
 
       <Container>
         <Row>
-          <Col>
-           <div className="p-3 my-2 rounded" style={{background: 'red'}}>
-              <Toast>
-                <ToastBody>
-                Nosso site de filmes foi criado para pessoas viciadas e apaixonadas por filmes. No nosso site você fica sabendo quais filmes são lançados, os que estão no cinema e aqueles que já estão em DVD, para você assistir em casa.
+          <Col className="">
+            <div className="" style={{background: ''}}>
+              <Toast  style={{background: 'rgb(233 30 99)'}}>
+                <ToastBody style={{color: 'white'}}>
+                Nosso site de filmes foi criado para pessoas viciadas e apaixonadas por filmes. No nosso site você fica sabendo quais filmes são lançados, os que estão no cinema e aqueles que já estão em DVD, para você assistir em casa.  No nosso site é entreter o mundo, e é por isso que levamos até você os melhores filmes para computadores, notebooks, celulares, tablets ou outros dispositivos. Nossos clientes controlam o que querem ver e quando, sem anúncios, com uma única assinatura.  Os filmes podem nos ajudar a encontrar respostas, a compartilhar sentimentos e a aprender um pouco mais sobre nós mesmos e o mundo.
                </ToastBody>
               </Toast>
             </div>
           </Col>
+         
+         
         </Row>
-        <Row>
-          <Col>
-            <div className="p-3 my-2 rounded bg-docs-transparent-grid" style={{background: '#24262b' }}>
-              <Toast>
-                <ToastBody>
-                No nosso site é entreter o mundo, e é por isso que levamos até você os melhores filmes para computadores, notebooks, celulares, tablets ou outros dispositivos. Nossos clientes controlam o que querem ver e quando, sem anúncios, com uma única assinatura.
-        </ToastBody>
-              </Toast>
-            </div>
-          </Col>
-          <Col>
-            <div className="p-3 my-2 rounded bg-docs-transparent-grid" style={{background: '#24262b'}}>
-              <Toast>
-                <ToastBody>
-                 No nosso site é entreter o mundo, e é por isso que levamos até você os melhores filmes para computadores, notebooks, celulares, tablets ou outros dispositivos. Nossos clientes controlam o que querem ver e quando, sem anúncios, com uma única assinatura.
-               </ToastBody>
-              </Toast>
-            </div>
-          </Col>
-        </Row>
-
-
+        <hr style={{background: 'rgb(233 30 99)'}}></hr>
+        <div className="p-3 my-2 rounded">
+           <div>
+           <Toast>
+           <ToastBody>
+           This is a toast on a white background — check it out!
+           </ToastBody>
+           </Toast>
+           </div>
+     </div>
       </Container>
 
     </div>
