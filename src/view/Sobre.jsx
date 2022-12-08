ed105643b063bf1f893daf3106e59afd2c2a9bee
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2022-12-08 20:28:48 -0300

    Update Sobre.jsx

--- src/view/Sobre.jsx
+++ src/view/Sobre.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, Row, Col, Toast, ToastHeader, ToastBody } from 'reactstrap';
+import { Container, Row, Col, Toast, ToastBody, Card, setEmail} from 'reactstrap';
 import "./css/Sobre.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/Contato.css"
 
 const Sobre = () => {
   document.title = "Sobre"
@@ -23,13 +25,59 @@ const Sobre = () => {
          <h1 className="p-3">Nossa <span>Equipe</span></h1>
          <div className="p-3 my-2 rounded">
            <div>  
-           <Toast>
+           <Card className="cd" >
            <ToastBody>
-           This is a toast on a white background — check it out!
+           <img className="img" src="https://avatars.githubusercontent.com/u/85020469?v=4" alt=""/>
+            <p className="p-3 my-2">JUAN SALES</p>
            </ToastBody>
-           </Toast>
+           </Card>
            </div>
          </div>
+
+         <div className="p-3 my-2 rounded">
+           <div>  
+           <Card className="cd" >
+           <ToastBody>
+           <img className="img" src="https://media-exp1.licdn.com/dms/image/C4E03AQH4_PQQ_J7lXA/profile-displayphoto-shrink_800_800/0/1625260631370?e=1675900800&v=beta&t=l8zBK_oePLFsTcfyvZn_GxG5HOQCKoZfBoiw8Ln_OX4" alt=""/>
+            <p className="p-3 my-2">LUAN ALVES</p>
+           </ToastBody>
+           </Card>
+           </div>
+         </div>
+
+         <div className="p-3 my-2 rounded">
+           <div>  
+           <Card className="cd" >
+           <ToastBody>
+           <img className="img" src="https://media-exp1.licdn.com/dms/image/D4D03AQE0mSVnQz6Beg/profile-displayphoto-shrink_800_800/0/1665256052199?e=1675900800&v=beta&t=n1Kc7fCblYTnDNzKqI8DKmCIQ88_iIxgGrozmpTaEkk" alt=""/>
+            <p className="p-3 my-2">LUÍS HENRIQUE</p>
+           </ToastBody>
+           </Card>
+           </div>
+         </div>
+
+         <div className="p-3 my-2 rounded">
+           <div>  
+           <Card className="cd" >
+           <ToastBody>
+           <img className="img" src="https://project-front-enders.vercel.app/img/pedro.jpg" alt=""/>
+            <p className="p-3 my-2">Pedro Souza</p>
+           </ToastBody>
+           </Card>
+           </div>
+         </div>
+
+         <div className="p-3 my-2 rounded">
+           <div>  
+           <Card className="cd">
+           <ToastBody>
+           <img className="img" src="https://nossosquad.vercel.app/img/patrick.jpg" alt=""/>
+            <p className="p-3 my-2">PATRICK COSTA</p>
+           </ToastBody>
+           </Card>
+           </div>
+         </div>
+         
       </Container>
     </div>
   )
