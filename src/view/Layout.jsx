96f0b520e4f9a78355375247fb6296ea06eff341
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-12-10 19:17:35 -0300

    Header, menu e logo atualizado

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -1,73 +1,50 @@
 import { Outlet } from "react-router-dom";
-import { Navbar, NavbarBrand, Button, Row, } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./css/Layout.css";
-import Logo from "./media/logo.png"
-
+import { Navbar, NavbarBrand, Button, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./css/Layout.module.css";
+import Logo from "./media/logo.png";
 
 const Layout = () => {
-    return (
-        <>
+  return (
+    <>
+      <Navbar className={styles.header}>
+        <Row className={styles.row}>
+          <div className={styles.navMenu}>
+            <NavbarBrand href="/">
+              <img className={styles.navLogo} alt="DevPlay TV" src={Logo} />
+            </NavbarBrand>
+            <Button className={styles.btn} color="none" href="/" tag="a">
+              <a>
+              <span style={{ color: "#e91e63" }}>&#123;</span> Home <span style={{ color: "#e91e63" }}>&#125;</span>
+              </a>
+            </Button>{" "}
+            <Button className={styles.btn} color="none" href="/Filmes" tag="a">
+              <a><span style={{ color: "#e91e63" }}>&#123;</span> Filmes <span style={{ color: "#e91e63" }}>&#125;</span></a>
+            </Button>{" "}
+            <Button className={styles.btn} color="none" href="/Sobre" tag="a">
+              <a><span style={{ color: "#e91e63" }}>&#123;</span> Sobre <span style={{ color: "#e91e63" }}>&#125;</span></a>
+            </Button>{" "}
+            <Button className={styles.btn} color="none" href="/Contato" tag="a">
+              <a><span style={{ color: "#e91e63" }}>&#123;</span> Contato <span style={{ color: "#e91e63" }}>&#125;</span></a>
+            </Button>{" "}
+            <div className={styles.searchBox}>
+              <input
+                className={styles.searchTxt}
+                type="text"
+                name=""
+                placeholder="Search"
+              />
+              <a className={styles.searchBtn} href="#">
+                <i><a></a></i>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Navbar>
 
-            <Navbar className="Header">
-
-                <Row className="row">
-                    <NavbarBrand className="Navlogo" href="/">
-                        <img
-                            alt="DevPlay TV"
-                            src={Logo}
-                            style={{
-                                height: 169,
-                                width: 285,
-                            }} />
-                    </NavbarBrand>
-                    <div className="NavMenu">
-                        <Button className="Btn"
-                            color="none"
-                            href="/"
-                            tag="a">
-                            <a>Home</a>
-                        </Button>
-                        {' '}
-                        <Button className="Btn"
-                            color="none"
-                            href="/Filmes"
-                            tag="a">
-                            <a>Filmes</a>
-                        </Button>
-                        {' '}
-                        <Button className="Btn"
-                            color="none"
-                            href="/Sobre"
-                            tag="a">
-                            <a>Sobre</a>
-                        </Button>
-                        {' '}
-                        <Button className="Btn"
-                            color="none"
-                            href="/Contato"
-                            tag="a">
-                            <a>Contato</a>
-                        </Button>
-                        {' '}
-                        <div className="SearchBox">
-                            <input className="SearchTxt" type="text" name="" placeholder="Search" />
-                            <a className="SearchBtn" href="#">
-                            <i className="fa fa-search" aria-hidden="true"></i> 
-                            </a>
-                            
-                        </div>
-
-                    </div>
-
-                </Row>
-
-            </Navbar>
-
-            <Outlet />
-
-        </>
-    )
+      <Outlet />
+    </>
+  );
 };
 
-export default Layout; 
+export default Layout;
