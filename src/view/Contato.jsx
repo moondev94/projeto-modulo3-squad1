63bab5e3108687e71539a64dd5408862afcb704b
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-12-12 20:00:57 -0300

    Update Contato.jsx

--- src/view/Contato.jsx
+++ src/view/Contato.jsx
@@ -75,7 +75,7 @@ const Contato = () => {
        Enviar
       </Button>
       <Modal isOpen={modal} toggle={toggle} >
-        <ModalHeader toggle={toggle}>Mensagem enviada!</ModalHeader>
+        <span toggle={toggle}>Mensagem enviada!</span>
         <ModalBody>
           Olá {name}, em breve retornaremos sua mensagem para seu e-mail {email}, aguarde em até 24 horas.
         </ModalBody>
