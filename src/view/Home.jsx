ba3aad28814c76aeb6407f4d1cabf3f558fd14b8
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-12-12 03:40:40 -0300

    Lançamentos e Top 10 adicionados

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -6,8 +6,8 @@ import {
   CarouselIndicators,
   CarouselCaption,
   Container,
-  Col,
   Row,
+  Col,
   Card,
   CardBody,
   CardTitle,
@@ -21,20 +21,17 @@ import Top3 from "./imagens/top3.jpg";
 const items = [
   {
     src: "https://hbomax-images.warnermediacdn.com/2021-11/hero_dsk_0.jpg?host=wme-hbomax-drupal-prod.s3.amazonaws.com&w=2800",
-    altText: "Slide 1",
-    caption: "Slide 1",
+    
     key: 1,
   },
   {
     src: "https://i.ytimg.com/vi/8d2EmV46mmo/maxresdefault.jpg",
-    altText: "Slide 2",
-    caption: "Slide 2",
+    
     key: 2,
   },
   {
     src: "https://hbomax-images.warnermediacdn.com/images/GYGP7pwQv_ojDXAEAAAFc/tileburnedin?size=1280x720&partner=hbomaxcom&v=bd4e8b8745a1fcccbb19d76012ae572e&host=art-gallery.api.hbo.com&language=pt-br&w=1280",
-    altText: "Slide 3",
-    caption: "Slide 3",
+    
     key: 3,
   },
 ];
@@ -81,8 +78,9 @@ function Home(args) {
   });
   document.title = "Home";
   return (
-    <Row className={styles.homePage}>
-      <Row className={styles.slideCarousel}>
+    
+    <Container className={styles.homePage}>
+      <Col className={styles.slideCarousel}>
         <Carousel
           activeIndex={activeIndex}
           next={next}
@@ -106,50 +104,74 @@ function Home(args) {
             onClickHandler={next}
           />
         </Carousel>
-      </Row>
-     
-      <Row className={styles.spotLight}>
-        
-        <Card className={styles.cd}>
-          <img className={styles.img} alt="Card" src={Top1} />
+      </Col>
 
-         
+      <Col>
+        <h2><span>#</span> Lançamentos<span></span></h2>
+        <p></p>
+        <Col className={styles.card}>
+          <div className="col-sm px-4">
+            <img alt="Card" src={Top1} />
             <h5>Vingadores: Ultimato</h5>
-            <div className={styles.cardTxt}>
               <a>
                 Após Thanos eliminar metade das criaturas vivas em Vingadores:
                 Guerra Infinita, os heróis precisam... Ler mais
               </a>
-            </div>
-          </Card>
-          <Card className={styles.cd}>
-          <img className={styles.img} alt="Card" src={Top2} />
-          
+            
+          </div>
+          <div className="col-sm px-4">
+            <img alt="Card" src={Top2} />
             <h5>Liga da Justiça</h5>
-            <div className={styles.cardTxt}>
               <a>
                 impulsionado pela restauração de sua fé na humanidade e
                 inspirado pelo ato altruísta do Superman (Henry Cavill), Bruce
                 Wayne (Ben Affleck) convoca sua nova aliada Diana Prince (Gal
                 Gadot)... Ler mais
               </a>
-            </div>
-          </Card>
-          <Card className={styles.cd}>
-          <img className={styles.img} alt="Card" src={Top3} />
-          
+            
+          </div>
+          <div className="col-sm px-4">
+            <img alt="Card" src={Top3} />
             <h5>Hereditário</h5>
-            <div className={styles.cardTxt}>
+            
               <a>
                 Após a morte da reclusa avó, a família Graham começa a desvendar
                 algumas coisas. Mesmo após a partida da matriarca... Ler mais
               </a>
-            </div>
-          </Card>
-          </Row>
-          </Row>
+            
+          </div>
+        </Col>
+      </Col>
+      <Col className={styles.top10}>
+      <h2><span>#</span> TOP<span>10</span></h2>
+        <p></p>
+        <Col className="col-sm px-0">
+        <ul>
+          <li><span>1.</span> Top Gun: Maverick</li>
+          <p></p>
+          <li><span>2.</span> Elvis</li>
+          <p></p>
+          <li><span>3.</span> Sonic 2 - O Filme</li>
+          <p></p>
+          <li><span>4.</span> Arremessando Alto</li>
+          <p></p>
+          <li><span>5.</span> Red: Crescer É uma Fera</li>
+          <p></p>
+          <li><span>6.</span> Batman</li>
+          <p></p>
+          <li><span>7.</span> Continência ao Amor</li>
+          <p></p>
+          <li><span>8.</span> Doutor Estranho no Multiverso da Loucura</li>
+          <p></p>
+          <li><span>9.</span> O Projeto Adam</li>
+          <p></p>
+          <li><span>10.</span> Animais Fantásticos: Os Segredos de Dumbledore</li>
         
-    
+        </ul>
+        </Col>
+
+      </Col>
+    </Container>
   );
 }
 
