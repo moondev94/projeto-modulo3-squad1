865c4dfc28a5a6e6e0ac8fd94a4e01570d9fcb6e
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-12-12 03:39:08 -0300

    Layout update

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -10,22 +10,35 @@ const Layout = () => {
       <Navbar className={styles.header}>
         <Row className={styles.row}>
           <div className={styles.navMenu}>
-            <NavbarBrand href="/">
-              <img className={styles.navLogo} alt="DevPlay TV" src={Logo} />
+            <NavbarBrand className={styles.navLogo} href="/">
+              <img
+                alt="DevPlay TV"
+                src={Logo}
+              />
             </NavbarBrand>
             <Button className={styles.btn} color="none" href="/" tag="a">
               <a>
-              <span style={{ color: "#e91e63" }}>&#123;</span> Home <span style={{ color: "#e91e63" }}>&#125;</span>
+                <span style={{ color: "#e91e63" }}>&#123;</span> Home{" "}
+                <span style={{ color: "#e91e63" }}>&#125;</span>
               </a>
             </Button>{" "}
             <Button className={styles.btn} color="none" href="/Filmes" tag="a">
-              <a><span style={{ color: "#e91e63" }}>&#123;</span> Filmes <span style={{ color: "#e91e63" }}>&#125;</span></a>
+              <a>
+                <span style={{ color: "#e91e63" }}>&#123;</span> Filmes{" "}
+                <span style={{ color: "#e91e63" }}>&#125;</span>
+              </a>
             </Button>{" "}
             <Button className={styles.btn} color="none" href="/Sobre" tag="a">
-              <a><span style={{ color: "#e91e63" }}>&#123;</span> Sobre <span style={{ color: "#e91e63" }}>&#125;</span></a>
+              <a>
+                <span style={{ color: "#e91e63" }}>&#123;</span> Sobre{" "}
+                <span style={{ color: "#e91e63" }}>&#125;</span>
+              </a>
             </Button>{" "}
             <Button className={styles.btn} color="none" href="/Contato" tag="a">
-              <a><span style={{ color: "#e91e63" }}>&#123;</span> Contato <span style={{ color: "#e91e63" }}>&#125;</span></a>
+              <a>
+                <span style={{ color: "#e91e63" }}>&#123;</span> Contato{" "}
+                <span style={{ color: "#e91e63" }}>&#125;</span>
+              </a>
             </Button>{" "}
             <div className={styles.searchBox}>
               <input
@@ -35,9 +48,10 @@ const Layout = () => {
                 placeholder="Search"
               />
               <a className={styles.searchBtn} href="#">
-                <i><a></a></i>
+              <i class="bi bi-search"></i>
               </a>
-            </div>
+              </div>
+        
           </div>
         </Row>
       </Navbar>
