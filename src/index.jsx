4e06364575787b32337de4e21c67f18b61e1976d
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-12-11 23:12:58 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import Contato from "./view/Contato";
 import Sobre from "./view/Sobre";
 import NoPage from "./view/NoPage";
 import Footer from "./view/Footer";
-import Qst from "./view/Faq";
 
 export default function App() {
   return (
@@ -20,7 +19,6 @@ export default function App() {
           <Route path="Sobre" element={<Sobre />} />
           <Route path="Contato" element={<Contato />} />
           <Route path="*" element={<NoPage />} />
-          <Route path="Qst" element={<Qst />} />
         </Route>
       </Routes>
     </BrowserRouter>
