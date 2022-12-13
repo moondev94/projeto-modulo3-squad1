1b0b149f147d019db91b12cc1a7306699db95065
Juan Sales <85020469+Juangsales@users.noreply.github.com>
2022-12-13 01:05:02 -0300

    Update Contato.jsx

--- src/view/Contato.jsx
+++ src/view/Contato.jsx
@@ -3,13 +3,11 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Card, FormGroup, Label, Input, Form } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/Contato.css"
-
 const Contato = () => {
   document.title="Contato";
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [modal, setModal] = useState(false);
-
   const toggle = () => setModal(!modal);
   return (
     <div className="corpoContato">
@@ -19,10 +17,11 @@ const Contato = () => {
   <Form className="bg-#24262b"
     fluid
   >
-    
+
+      <h1>Entre em <span>contato</span></h1>
       <h1>Entre em contato</h1>
-      
-      
+
+
       <FormGroup>
       <Label for="name">
       Nome
@@ -67,9 +66,7 @@ const Contato = () => {
       name="text"
       type="textarea"
     />
-
 </FormGroup>
-
 <div>
       <Button color="danger" onClick={toggle}>
        Enviar
