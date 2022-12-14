584661defbd28e2d93169328d7f3dbebaeecf425
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2022-12-14 01:06:56 -0300

    Página de erro adicionada

--- src/view/NoPage.jsx
+++ src/view/NoPage.jsx
@@ -1,5 +1,20 @@
+import styles from "./css/NoPage.module.css"
+
 const NoPage = () => {
-    return <h1>404</h1>;
-  };
-  
-  export default NoPage;
+  return (
+  <>
+    <div id={styles.notFound}>
+      <div className={styles.notFound}>
+        <div className={styles.notFound404}>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+        </div>
+        <a href="/Contato">Report</a>
+        <a href="/">Homepage</a>
+      </div>
+    </div>
+  </>
+  )
+};
+
+export default NoPage;
